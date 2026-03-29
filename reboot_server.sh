e6a33eae2f9ee22d3db4ac1f090c89b86ab4cc5e
Freddie Mac - KD5FMU <fred.mcguire.jr@gmail.com>
2026-03-29 12:21:09 -0500

    Add files via upload

---
+++ reboot_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Filename: /etc/asterisk/local/reboot_server.sh
+# Purpose: Reboots the server when triggered by a DTMF command.
+# Author: Ham Radio Crusader
+
+# Log the reboot action for debugging purposes
+echo "$(date): Reboot command received via DTMF." >> /var/log/asterisk/reboot_command.log
+
+# Reboot the system
+/sbin/reboot
+
+
